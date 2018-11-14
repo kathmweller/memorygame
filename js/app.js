0e413ec624c5a7ca1b053deb4de3f37ede497b1e
kathmweller <kathmweller@gmail.com>
2018-11-13 21:58:59 -0600

    Fixed the restart functionality.

--- js/app.js
+++ js/app.js
@@ -13,8 +13,25 @@ var cards = ['fa-diamond', 'fa-diamond',
 
 var movesCounter = document.querySelector('.moves');
 
-function generateCard(card) {
-    return `<li class='card' data-card="${card}"><i class="fa ${card}"></i></li>`;
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+function initGame() {
+    var deck = document.querySelector('.deck');
+    var cardHTML = shuffle(cards).map(function(card) {
+        return generateCard(card);
+    });
+
+    movesCounter.innerHTML = 0;
+    deck.innerHTML= cardHTML.join('');
 }
 
 /*
@@ -39,26 +56,8 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-function initGame() {
-    var deck = document.querySelector('.deck');
-    var cardHTML = shuffle(cards).map(function(card) {
-        return generateCard(card);
-    });
-
-    movesCounter.innerHTML = 0;
-    deck.innerHTML= cardHTML.join('');
+function generateCard(card) {
+    return `<li class='card' data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
 
 initGame();
@@ -102,9 +101,14 @@ restartGame.addEventListener('click', function(e) {
     
     clearTimeout(t);
     movesCounter.innerHTML = 0;
+    newCardSort = shuffle(cards);
     
-    allCards.forEach(function(card) {
+    allCards.forEach(function(card,index) {        
+        card.childNodes[0].classList.remove(card.getAttribute('data-card'));
+        card.childNodes[0].classList.add(newCardSort[index]);
+        card.setAttribute('data-card', newCardSort[index]);
         card.classList.remove('open', 'show', 'match');
+        index++;
     });
 
     clicks = 0;
